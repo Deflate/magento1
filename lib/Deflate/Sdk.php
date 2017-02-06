1fec6bdb205abe490359269fbf7b179f33e9bf41
Dave Macaulay <dave@gene.co.uk>
2017-02-06 11:06:36 +0000

    Update SDK to latest

--- lib/Deflate/Sdk.php
+++ lib/Deflate/Sdk.php
@@ -10,14 +10,25 @@ class Deflate_Sdk
     /**
      * The URL for the API
      */
-    const API_URL = 'http://deflate.io/api/v1/';
+    const API_URL = 'https://api.deflate.io/v1/';
 
     /**
      * The current SDK version
      */
     const SDK_VERSION = '1.0.0';
 
+    /**
+     * API Key
+     *
+     * @var string
+     */
     private $apiKey;
+
+    /**
+     * API secret
+     *
+     * @var string
+     */
     private $apiSecret;
 
     /**
@@ -34,53 +45,115 @@ class Deflate_Sdk
         $this->apiSecret = $apiSecret;
 
         // Check we can login with the details provided
-        if(!$this->account()) {
+        if (!$this->account()) {
             throw new Exception('Unable to connect to Deflate API using provided credentials.');
         }
     }
 
     /**
-     * Compress a single image
+     * Grab the account information
      *
-     * @param $image
-     * @param $type
-     * @param bool|true $wait
-     * @param bool|false $callback
-     * @param bool|false $custom
      * @return bool|mixed
-     * @throws Exception
+     */
+    public function account()
+    {
+        if ($response = $this->_request('account')) {
+            if (isset($response->success) && $response->success == true) {
+                return $response->account;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Make a request to the API
+     *
+     * @param            $action
+     * @param bool|false $data
+     *
+     * @return mixed
+     */
+    private function _request($action, $data = false)
+    {
+        // Get cURL resource
+        $curl = curl_init();
+
+        // Start building our body
+        $body = array(
+            'auth' => array(
+                'api_key'    => $this->apiKey,
+                'api_secret' => $this->apiSecret
+            )
+        );
+
+        // Merge in the data
+        if ($data !== false && is_array($data)) {
+            $body = array_merge($body, $data);
+        }
+
+        // Set the basic CURL requests options
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL            => self::API_URL . $action,
+            CURLOPT_USERAGENT      => 'Deflate SDK - Version ' . self::SDK_VERSION,
+            CURLOPT_POST           => 1,
+            CURLOPT_POSTFIELDS     => json_encode($body)
+        ));
+
+        // Send the request & save response to $resp
+        $resp = curl_exec($curl);
+
+        // Close request to clear up some resources
+        curl_close($curl);
+
+        return json_decode($resp);
+    }
+
+    /**
+     * Compress an individual image
+     *
+     * @param      $image
+     * @param      $type
+     * @param bool $id
+     * @param bool $wait
+     * @param bool $callback
+     * @param bool $custom
+     *
+     * @return bool|mixed
+     * @throws \Exception
      */
     public function compress($image, $type, $id = false, $wait = true, $callback = false, $custom = false)
     {
         // Build our request
         $request = array(
             'image' => $image,
-            'type' => $type,
-            'wait' => $wait
+            'type'  => $type,
+            'wait'  => $wait
         );
 
         // Include an ID with the request
-        if($id !== false) {
+        if ($id !== false) {
             $request['id'] = $id;
         }
 
         // Verify that we have a callback
-        if($wait === false && $callback === false) {
+        if ($wait === false && $callback === false) {
             throw new Exception('A callback must be defined when you\'re not wanting to wait.');
         }
 
         // Include the fallback in the request
-        if($callback) {
+        if ($callback) {
             $request['callback'] = $callback;
         }
 
         // Pass any custom data over
-        if($custom) {
+        if ($custom) {
             $request['custom'] = $custom;
         }
 
-        if($response = $this->_request('deflate', $request)) {
-            if(isset($response->success) && $response->success == true) {
+        if ($response = $this->_request('deflate', $request)) {
+            if (isset($response->success) && $response->success == true) {
                 return $response;
             }
         }
@@ -91,11 +164,12 @@ class Deflate_Sdk
     /**
      * Attempt to compress multiple images
      *
-     * @param $images
-     * @param $type
-     * @param bool|true $wait
+     * @param            $images
+     * @param            $type
+     * @param bool|true  $wait
      * @param bool|false $callback
      * @param bool|false $custom
+     *
      * @return bool|mixed
      * @throws Exception
      */
@@ -104,44 +178,28 @@ class Deflate_Sdk
         // Build our request
         $request = array(
             'images' => $images,
-            'type' => $type,
-            'wait' => $wait
+            'type'   => $type,
+            'wait'   => $wait
         );
 
         // Verify that we have a callback
-        if($wait === false && $callback === false) {
+        if ($wait === false && $callback === false) {
             throw new Exception('A callback must be defined when you\'re not wanting to wait.');
         }
 
         // Include the fallback in the request
-        if($callback) {
+        if ($callback) {
             $request['callback'] = $callback;
         }
 
         // Pass any custom data over
-        if($custom) {
+        if ($custom) {
             $request['custom'] = $custom;
         }
 
-        if($response = $this->_request('deflate', $request)) {
-            if(isset($response->success) && $response->success == true) {
+        if ($response = $this->_request('deflate', $request)) {
+            if (isset($response->success) && $response->success == true) {
                 return $response;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Grab the account information
-     *
-     * @return bool|mixed
-     */
-    public function account()
-    {
-        if($response = $this->_request('account')) {
-            if(isset($response->success) && $response->success == true) {
-                return $response->account;
             }
         }
 
@@ -155,8 +213,8 @@ class Deflate_Sdk
      */
     public function limit()
     {
-        if($response = $this->_request('limit')) {
-            if(isset($response->limit) && $response->limit == true) {
+        if ($response = $this->_request('limit')) {
+            if (isset($response->limit) && $response->limit == true) {
                 return $response->limit;
             }
         }
@@ -167,22 +225,24 @@ class Deflate_Sdk
     /**
      * Return the supported image formats
      *
+     * @param bool $type
+     *
      * @return bool|mixed
      */
     public function supported($type = false)
     {
-        if($response = $this->_request('supported')) {
-            if(isset($response->success) && $response->success == true) {
-                switch($type) {
+        if ($response = $this->_request('supported')) {
+            if (isset($response->success) && $response->success == true) {
+                switch ($type) {
                     case 'extensions':
                         return $response->extensions;
-                    break;
+                        break;
                     case 'mime':
                         return $response->types;
-                    break;
+                        break;
                     default:
                         return $response;
-                    break;
+                        break;
                 }
             }
         }
@@ -198,48 +258,5 @@ class Deflate_Sdk
     public function callbackResponse()
     {
         return json_decode(file_get_contents('php://input'));
-    }
-
-    /**
-     * Make a request to the API
-     *
-     * @param $action
-     * @param bool|false $data
-     * @return mixed
-     */
-    private function _request($action, $data = false)
-    {
-        // Get cURL resource
-        $curl = curl_init();
-
-        // Start building our body
-        $body = array(
-            'auth' => array(
-                'api_key' => $this->apiKey,
-                'api_secret' => $this->apiSecret
-            )
-        );
-
-        // Merge in the data
-        if($data !== false && is_array($data)) {
-            $body = array_merge($body, $data);
-        }
-
-        // Set the basic CURL requests options
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => self::API_URL . $action,
-            CURLOPT_USERAGENT => 'Deflate SDK - Version ' . self::SDK_VERSION,
-            CURLOPT_POST => 1,
-            CURLOPT_POSTFIELDS => json_encode($body)
-        ));
-
-        // Send the request & save response to $resp
-        $resp = curl_exec($curl);
-
-        // Close request to clear up some resources
-        curl_close($curl);
-
-        return json_decode($resp);
     }
 }
